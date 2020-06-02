f23702971fcb196567dfd8ed3f61824b9520c961
Sean Wong <wotzhs@gmail.com>
2020-06-02 15:29:11 +0800

    order: implement create order service

---
+++ order/src/services/order.js
@@ -0,0 +1,27 @@
+import Order from "../models/order";
+import pool from "../db";
+
+class OrderService {
+	static async createOrder(order) {
+		if (order instanceof Order == false) {
+			return Error(Order.INCORRECT_INSTANCE_MSG);
+		}
+
+		try {
+			const res = await pool.query(
+				`
+				INSERT INTO orders (id, user_id, payee_id, description, amount, status)
+				VALUES ($1, $2, $3, $4, $5, $6)
+				RETURNING id;
+				`,
+				[ order.id, order.user_id, order.payee_id, order.description, order.amount, order.status ]
+			);
+
+			return res.rows[0];
+		} catch (e) {
+			return e;
+		}
+	}
+}
+
+export default OrderService;
