976525d88a58803ae7298cc1f2d166210a732752
Sean Wong <wotzhs@gmail.com>
2020-06-02 23:37:51 +0800

    event: add postgres connector for event-store service

---
+++ event-store/src/db.js
@@ -0,0 +1,12 @@
+import { Pool, types } from "pg";
+
+types.setTypeParser(1114, (str) => {
+	const utcStr = `${str}Z`;
+	return new Date(utcStr).toISOString();
+});
+
+const pool = new Pool({
+	connectionString: process.env.EVENT_DB_URL,
+});
+
+export default pool;
