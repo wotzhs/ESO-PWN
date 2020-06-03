62a07e882879b7f412ec5ec291505a9d89715afe
Sean Wong <wotzhs@gmail.com>
2020-06-03 16:47:35 +0800

    payment: publish message on defined channel on nats network post record creation in event-store db

--- event-store/src/services/index.js
+++ event-store/src/services/index.js
@@ -1,10 +1,24 @@
 import { v4 as uuidV4 } from "uuid";
 import pool from "../db";
+import sc from "../../clients/nats-streaming";
+
+const eventToChannelMapping = {
+	"order_created": "order.created",
+	"payment_confirmed": "payment.confirmed",
+	"payment_declined": "payment.declined",
+	"order_confirmed": "order.confirmed",
+	"order_cancelled": "order.cancelled",
+	"order_delivered": "order.delivered",
+};
 
 class Service {
 	static async createEvent({ event, aggregate_id, aggregate_type, event_data }) {
 		try {
-			console.log(JSON.parse(event_data));
+			const channel = eventToChannelMapping[event];
+			if (!channel) {
+				throw Error("invalid event");
+			}
+
 			const res = await pool.query(
 				`
 				INSERT INTO events (id, event, aggregate_id, aggregate_type, event_data)
@@ -13,6 +27,14 @@ class Service {
 				`,
 				[ uuidV4(), event, aggregate_id, aggregate_type, event_data ]
 			);
+
+			await new Promise((resolve, reject) => {
+				sc.publish(channel, event_data, (err, guid)=> {
+					if (err) reject(err);
+					resolve();
+				});
+			});
+
 			return res.rows[0];
 		} catch (e) {
 			return e;
