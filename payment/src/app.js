6cb93ee8aee69f2f9cf24538f40095e29e8ebd02
Sean Wong <wotzhs@gmail.com>
2020-06-03 16:57:15 +0800

    payment: activate nats streaming worker

---
+++ payment/src/app.js
@@ -0,0 +1,7 @@
+import sc from "../clients/nats-streaming";
+import NATSStreamingWorker from "../workers/nats-streaming";
+
+sc.on("connect", ()=> {
+	const natsWorker = new NATSStreamingWorker(sc);
+	natsWorker.activate();
+});
