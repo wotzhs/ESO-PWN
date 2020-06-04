52aee81a6cc9d922a52f4e54a2337539f0d1bf1a
Sean Wong <wotzhs@gmail.com>
2020-06-04 12:49:38 +0800

    order_update: process declined payments

--- order/src/workers/nats-streaming.js
+++ order/src/workers/nats-streaming.js
@@ -55,6 +55,43 @@ class NATSStreamingWorker {
 	}
 
 	processDeclinedPayment() {
+		const opts = this.#client.subscriptionOptions();
+		opts.setDeliverAllAvailable();
+		opts.setDurableName("durable_sub_order_confirmed");
+
+		const durableSub = this.#client.subscribe("payment.declined", opts);
+		durableSub.on("message", async (msg)=> {
+			const eventData = JSON.parse(msg.getData());
+
+			const eventPayload = {
+				event: "order_cancelled",
+				aggregate_id: eventData.event_id,
+				aggregate_type: "order",
+				event_data: JSON.stringify({ event_id: eventData.event_id }),
+			};
+
+			try {
+				// update order to confirmed
+				const res = OrderService.updateOrderStatusById({ id:eventData.event_id, status: "cancelled" });
+				if (res instanceof Error) {
+					console.log(res);
+					// TODO: publish to error channel and requeue to be reprocessed (not within assessment scope)
+				}
+
+				await new Promise((resolve, reject) => {
+					eventStoreService.createEvent(eventPayload, async (err, resp) => {
+						if (err) {
+							reject(err);
+						}
+
+						resolve();
+					});
+				});
+			} catch (e) {
+				console.log(e);
+				// TODO: publish to error channel and requeue to be reprocessed (not within assessment scope)
+			}
+		});
 	}
 
 	processConfirmedOrder() {
