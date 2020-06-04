b350007a3109a0af4a96ce8a0410e5cef51afc3f
Sean Wong <wotzhs@gmail.com>
2020-06-04 12:47:01 +0800

    order_update: fix predeclined payment prefixes is not correctly checked against

--- payment/clients/mocked-payment-provider.js
+++ payment/clients/mocked-payment-provider.js
@@ -2,17 +2,25 @@ import { v4 as uuidV4 } from "uuid";
 
 class MockedPaymentProvider {
 	#preApprovedPaymentsPrefixes = ["test"];
+	#preDeclinedPaymentsPrefixes = ["xxx"]
 
 	pay(eventData) {
-		for (let i=0; i<this.#preApprovedPaymentsPrefixes; i++) {
-			// check if payment description starts with pre approved keyword
+		// check if payment description starts with pre approved keyword
+		for (let i=0; i<this.#preApprovedPaymentsPrefixes.length; i++) {
 			if (eventData.description.startsWith(this.#preApprovedPaymentsPrefixes[i])) {
 				return { payment_ref: uuidV4() };
 			}
-
-			// else return random result
-			return Math.random() > 0.5 ? Error("payment declined") : { payment_ref: uuidV4() };
 		}
+
+		// check if payment description starts with pre declined keyword
+		for (let i=0; i<this.#preDeclinedPaymentsPrefixes.length; i++) {
+			if (eventData.description.startsWith(this.#preDeclinedPaymentsPrefixes[i])) {
+				return Error("payment declined");
+			}
+		}
+
+		// else return random result
+		return Math.random() > 0.5 ? Error("payment declined") : { payment_ref: uuidV4() };
 	}
 }
 
